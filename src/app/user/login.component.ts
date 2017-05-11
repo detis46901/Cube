bd1faff7d2abaa7264fa70bc1807c3e447118d76
detis46901 <detis46901@gmail.com>
2017-05-11 11:47:05 -0400

    New commit

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -7,7 +7,8 @@ import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
     //moduleId: module.id, (why does this not need to be there???)
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styleUrls: ['login.component.css']
 })
  
 export class LoginComponent implements OnInit {
