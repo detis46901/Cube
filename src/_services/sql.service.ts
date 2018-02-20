875750140e95125f601cf6db8fde1c34f1d0d433
HippieJosh <joshchurch1996@gmail.com>
2018-02-20 15:16:09 -0500

    Init develop

--- src/_services/sql.service.ts
+++ src/_services/sql.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Configuration } from '../_api/api.constants';
-import { MyCubeField } from '../_models/layer.model'
+import { MyCubeField } from '../_models/layer.model';
  
 @Injectable()
 export class SQLService {
