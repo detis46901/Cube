7054aa6fa1944af4e22d9fdf570c087c420c1555
Carey Stranahan <cstranahan@kokomo.org>
2020-06-03 21:50:12 -0400

    Update SDS Component

--- src/app/feature-modules/feature-modules/SDS/SDS.component.ts
+++ src/app/feature-modules/feature-modules/SDS/SDS.component.ts
@@ -122,6 +122,7 @@ export class SDSComponent implements OnInit {
         this.SDSConfig.moduleSettings.properties.forEach((x) => {
           if (x.stringType.name == "SDS Linked Field") {
             this.SDSConfig.linkedField = x.stringType.value;
+            this.SDSConfig.itemDataForm.dataForm.find((y) => y.field == this.SDSConfig.linkedField).visible = false
           }
           if (x.stringType.name == "Label") {
             this.SDSConfig.label = x.stringType.value;
@@ -149,8 +150,7 @@ export class SDSComponent implements OnInit {
   }
 
   public selectFeature(layer): boolean {
-    this.mapConfig.showDeleteButton = true;
-    //this.styleSelectedFeature(layer)
+    if (this.mapConfig.currentLayer.layerPermissions.edit) {this.mapConfig.showDeleteButton = true;}
     this.SDSConfig.itemDataForm.rowID = this.mapConfig.selectedFeature.get(
       "id"
     );
